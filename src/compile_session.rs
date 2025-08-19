458489c2e6381528448b581fded2ab9b4a917f12
nyathebo23 <franckhebo@gmail.com>
2025-08-19 13:40:38 +0100

    var stmt

--- src/compile_session.rs
+++ src/compile_session.rs
@@ -1,5 +1,5 @@
 use std::{process};
-use crate::{error_handler::{handle_error, ErrorType}};
+use crate::{error_handler::{handle_error, ErrorType}, parser::declarations::NIL};
 use crate::scanner::declarations::{TokenType};
 use crate::parser::{Parser};
 
@@ -46,10 +46,16 @@ impl CompileSession <'_> {
         self.next();
         let identifier = self.parser.current_token().clone();
         self.consume(TokenType::IDENTIFIER, "identifier");
-        self.consume(TokenType::EQUAL, "=");
-        let expr = self.parser.expression();
-        self.parser.set_variable(identifier.lexeme.to_string(), expr.evaluate());
-        self.consume(TokenType::SEMICOLON, ";");
+        let token = self.parser.current_token();
+        if token.token_type == TokenType::EQUAL {
+            self.next();
+            let expr = self.parser.expression();
+            self.parser.set_variable(identifier.lexeme.to_string(), expr.evaluate());
+        }
+        else {
+            self.parser.set_variable(identifier.lexeme.to_string(), Box::new(NIL));
+        }
+        self.consume(TokenType::SEMICOLON, ";"); 
     }
 
     fn next(&mut self) {
