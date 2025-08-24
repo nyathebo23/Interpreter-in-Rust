fcb32f1319140d7b725f3d73e209191fc8e8bf51
nyathebo23 <franckhebo@gmail.com>
2025-08-24 05:37:36 +0100

    function return stmt

--- src/function_manage.rs
+++ src/function_manage.rs
@@ -77,7 +77,7 @@ impl Function {
             Some(ret_val ) => ret_val,
             None => Box::new(NIL)
         };
-        println!("{}", ret_value.to_string());
+
         out_func_state.end_child_block();
         ret_value
     }
