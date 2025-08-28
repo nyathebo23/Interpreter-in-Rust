8091b302b70a78e4e782944eb69691db108633fe
nyathebo23 <franckhebo@gmail.com>
2025-08-28 18:38:32 +0100

    class init

--- src/class.rs
+++ src/class.rs
@@ -108,29 +108,27 @@ impl ToString for ClassInstance {
 
 impl Class {
     pub fn call(&self, params: &Vec<Box<dyn Expression>>, out_func_state: &mut BlockScopes, line: &u32) -> ClassInstance {
-        let instance = ClassInstance {
+        let mut instance = ClassInstance {
             class: Rc::new(self.clone()),
             attributes: Rc::new(RefCell::new(HashMap::new())) 
         };
         let this = String::from("this");
         
         if let Some(init_method) = &self.constructor {
-            let mut attributes_mut = instance.attributes.borrow_mut();
             let name = init_method.name.to_string();
             let instance_copy: Box<dyn Object> = Box::new(instance.clone());
             let mut func_copy = init_method.clone();
             func_copy.extra_map.insert(this.clone(), Rc::new(RefCell::new(instance_copy)));
             func_copy.call(params, out_func_state, line);
-            attributes_mut.insert(name, Rc::new(RefCell::new(Box::new(func_copy))));
+            instance.set(&name, Box::new(func_copy));
         }
         //let mut attrs = HashMap::new();
         for func in self.methods.iter() {
-            let mut attributes_mut = instance.attributes.borrow_mut();
             let name = func.name.to_string();
             let instance_copy: Box<dyn Object> = Box::new(instance.clone());
             let mut func_copy = func.clone();
             func_copy.extra_map.insert(this.clone(), Rc::new(RefCell::new(instance_copy)));
-            attributes_mut.insert(name, Rc::new(RefCell::new(Box::new(func_copy))));
+            instance.set(&name, Box::new(func_copy));
             // let func_obj: Box<dyn Object> = Box::new(func_copy);
             // attrs.insert(name, Rc::new(RefCell::new(func_obj)));
         }
